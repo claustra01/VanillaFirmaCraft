1fda793d0749da86136dac231202f8386025e48c
claustra01 <shinya430.g@gmail.com>
2023-08-15 01:51:05 +0900

    :adhesive_bandage: hide tfc ores

---
+++ kubejs/client_scripts/_hide_types.js
@@ -0,0 +1,24 @@
+
+
+global.tfcDisableRockList = [
+  "granite",
+  "diorite",
+  "gabbro",
+  "shale",
+  "claystone",
+  "limestone",
+  "conglomerate",
+  "dolomite",
+  "chert",
+  "chalk",
+  "rhyolite",
+  "basalt",
+  "andesite",
+  "dacite",
+  "quartzite",
+  "slate",
+  "phyllite",
+  "schist",
+  "gneiss",
+  "marble"
+]
---
+++ kubejs/client_scripts/hide_jei.js
@@ -0,0 +1,20 @@
+// priority: 0
+
+console.info('JEI item hiding...')
+
+onEvent('jei.hide.items', event => {
+	console.log("minecraft:stone")
+	// tfc-style ores
+	global.tfcStyleOreList.forEach(ore => {
+		global.tfcDisableRockList.forEach(rock => {
+			// tfc ore blocks
+			if (ore.isGraded) {
+				event.hide(`${ore.modId}:ore/poor_${ore.name}/${rock}`)
+				event.hide(`${ore.modId}:ore/normal_${ore.name}/${rock}`)
+				event.hide(`${ore.modId}:ore/rich_${ore.name}/${rock}`)
+			} else {
+				event.hide(`${ore.modId}:ore/${ore.name}/${rock}`)
+			}
+		})
+	})
+})
