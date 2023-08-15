bec11705ab8ba5384d84f458cbe5b621b076970a
claustra01 <shinya430.g@gmail.com>
2023-08-15 12:45:33 +0900

    :adhesive_bandage: hide tfc stone blocks

--- kubejs/client_scripts/hide_jei.js
+++ kubejs/client_scripts/hide_jei.js
@@ -3,10 +3,9 @@
 console.info('JEI item hiding...')
 
 onEvent('jei.hide.items', event => {
-	console.log("minecraft:stone")
 	// tfc-style ores
 	global.tfcStyleOreList.forEach(ore => {
-		global.tfcDisableRockList.forEach(rock => {
+		global.disableTfcRockList.forEach(rock => {
 			// tfc ore blocks
 			if (ore.isGraded) {
 				event.hide(`${ore.modId}:ore/poor_${ore.name}/${rock}`)
@@ -14,9 +13,24 @@ onEvent('jei.hide.items', event => {
 				event.hide(`${ore.modId}:ore/rich_${ore.name}/${rock}`)
 				event.hide(`${ore.modId}:ore/poor_${ore.name}`)
 				event.hide(`${ore.modId}:ore/rich_${ore.name}`)
+				event.hide(`${ore.modId}:deposit/${ore.name}/${rock}`)
 			} else {
 				event.hide(`${ore.modId}:ore/${ore.name}/${rock}`)
 			}
+		})
+	})
+	// tfc stone blocks
+	global.disableTfcRockList.forEach(rock => {
+		global.disableTfcStoneBlockList.forEach(type => {
+			let item = `tfc:rock/${type}/${rock}`
+			let isIgnored = false
+			global.disableIgnoreList.forEach(ignore => {
+				if (item == ignore) isIgnored = true
+			})
+			if (!isIgnored) event.hide(item)
+			event.hide(`tfc:rock/${type}/${rock}_slab`)
+			event.hide(`tfc:rock/${type}/${rock}_stairs`)
+			event.hide(`tfc:rock/${type}/${rock}_wall`)
 		})
 	})
 })
