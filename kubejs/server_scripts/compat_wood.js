432c164da9dcf626b92aa6dcb7d25fd1b86e10f0
claustra01 <shinya430.g@gmail.com>
2023-08-16 16:53:16 +0900

    :zap: tweak wood compat

--- kubejs/server_scripts/compat_wood.js
+++ kubejs/server_scripts/compat_wood.js
@@ -45,12 +45,8 @@ onEvent('recipes', event => {
     event.remove({output: `minecraft:${wood.vanilla}_fence_gate`})
     event.remove({output: `minecraft:${wood.vanilla}_boat`})
     // replace tfc-style recipes
-    event.remove({output: `tfc:wood/lumber/${wood.tfc}`})
     event.remove({output: `tfc:wood/planks/${wood.tfc}_log_fence`})
-    event.remove({output: `tfc:wood/support/${wood.tfc}`})
     event.remove({output: `firmalife:wood/jarbnet/${wood.tfc}`})
-    event.shapeless(`8x tfc:wood/lumber/${wood.tfc}`, [`#minecraft:${wood.vanilla}_logs`, "#tfc:saws"]).damageIngredient("#tfc:saws", 1)
-    event.shapeless(`4x tfc:wood/lumber/${wood.tfc}`, [`tfc:wood/planks/${wood.tfc}`, "#tfc:saws"]).damageIngredient("#tfc:saws", 1)
     event.shaped(`8x tfc:wood/planks/${wood.tfc}_log_fence`, [
       "LWL",
       "LWL"
@@ -58,13 +54,6 @@ onEvent('recipes', event => {
       "L": `minecraft:${wood.vanilla}_log`,
       "W": `tfc:wood/lumber/${wood.tfc}`,
     })
-    event.shaped(`8x tfc:wood/support/${wood.tfc}`, [
-      "LL",
-      "S "
-    ], {
-      "L": `#minecraft:${wood.vanilla}_logs`,
-      "S": "#tfc:saws"
-    }).damageIngredient("#tfc:saws", 1)
     event.shaped(`2x firmalife:wood/jarbnet/${wood.tfc}`, [
       "L  ",
       "RWW",
@@ -74,5 +63,13 @@ onEvent('recipes', event => {
       "W": `tfc:wood/lumber/${wood.tfc}`,
       "R": "tfc:metal/rod/brass"
     })
+	})
+})
+
+onEvent('tags.items', event => {
+  wootPairs.forEach(wood => {
+  	// add log tags
+    event.add(`tfc:${wood.tfc}_logs`, [`minecraft:${wood.vanilla}_log`, `minecraft:stripped_${wood.vanilla}_log`])
+    event.add(`tfc:${wood.tfc}_logs`, [`minecraft:${wood.vanilla}_wood`, `minecraft:stripped_${wood.vanilla}_wood`])
 	})
 })
