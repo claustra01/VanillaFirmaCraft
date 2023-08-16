1b3e5e29bd7ab73b04b487de3ef1a7328e479801
claustra01 <shinya430.g@gmail.com>
2023-08-17 01:00:25 +0900

    :construction: generate native copper veins

--- assets_generator/ores.js
+++ assets_generator/ores.js
@@ -1,305 +1,91 @@
-const Jimp = require("jimp")
+const fs = require('fs')
+const Jimp = require('jimp')
 
-const oreType = [
+const newOreType = [
   // tfc graded ores
+  // vein: [rarity, maxY, minY, maxSize, minSize]
   {
     "name": "native_copper",
     "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "native_gold",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "hematite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "native_silver",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "cassiterite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "bismuthinite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "garnierite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "malachite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "magnetite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "limonite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "sphalerite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  {
-    "name": "tetrahedrite",
-    "modId": "tfc",
-    "isGraded": true
-  },
-  // tfc-metallum graded ores
-  {
-    "name": "bauxite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },  {
-    "name": "bertrandite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "cobaltite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "kernite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "galena",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "monazite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "native_osmium",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "native_iridium",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "native_platinum",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "rutile",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "stibnite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "uraninite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  {
-    "name": "wolframite",
-    "modId": "tfc_metallum",
-    "isGraded": true
-  },
-  // firmalife graded ores
-  {
-    "name": "chromite",
-    "modId": "firmalife",
-    "isGraded": true
-  },
-  // tfc non-graded ores
-  {
-    "name": "bituminous_coal",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "lignite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "kaolinite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "gypsum",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "graphite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "sulfur",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "cinnabar",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "cryolite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "saltpeter",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "sylvite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "borax",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "halite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "amethyst",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "diamond",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "emerald",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "lapis_lazuli",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "opal",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "pyrite",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "ruby",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "sapphire",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  {
-    "name": "topaz",
-    "modId": "tfc",
-    "isGraded": false
-  },
-  // tfc-metallum non-graded ores
-  {
-    "name": "certus_quartz",
-    "modId": "tfc_metallum",
-    "isGraded": false
+    "isGraded": true,
+    "veinData": {
+      "minecraft:overworld": [120, 72, 0, 1000, 700]
+    }
   }
 ]
 
-const rockType = [
+const newRockType = [
   // overworld
   {
     "name": "stone",
     "hardness": 1.5,
-    "resistance": 6
+    "resistance": 6,
+    "dimension": "minecraft:overworld",
+    "origin": "minecraft:stone"
   },
   {
     "name": "deepslate",
     "hardness": 3,
-    "resistance": 6
+    "resistance": 6,
+    "dimension": "minecraft:overworld",
+    "origin": "minecraft:deepslate"
   },
   {
     "name": "granite",
     "hardness": 1.5,
-    "resistance": 6
+    "resistance": 6,
+    "dimension": "minecraft:overworld",
+    "origin": "minecraft:granite"
   },
   {
     "name": "diorite",
     "hardness": 1.5,
-    "resistance": 6
+    "resistance": 6,
+    "dimension": "minecraft:overworld",
+    "origin": "minecraft:diorite"
   },
   {
     "name": "andesite",
     "hardness": 1.5,
-    "resistance": 6
+    "resistance": 6,
+    "dimension": "minecraft:overworld",
+    "origin": "minecraft:andesite"
   },
   // nether
   {
     "name": "netherrack",
     "hardness": 0.4,
-    "resistance": 0.4
+    "resistance": 0.4,
+    "dimension": "minecraft:the_nether",
+    "origin": "minecraft:netherrack"
   },
   {
     "name": "basalt",
     "hardness": 1.25,
-    "resistance": 4.2
+    "resistance": 4.2,
+    "dimension": "minecraft:the_nether",
+    "origin": "minecraft:basalt"
   },
   {
     "name": "blackstone",
     "hardness": 1.5,
-    "resistance": 6
+    "resistance": 6,
+    "dimension": "minecraft:the_nether",
+    "origin": "minecraft:blackstone"
   },
   // the end
   {
     "name": "endstone",
     "hardness": 3,
-    "resistance": 9
+    "resistance": 9,
+    "dimension": "minecraft:the_end",
+    "origin": "minecraft:end_stone"
   }
 ]
 
-oreType.forEach(ore => {
-  rockType.forEach(async rock => {
+// generate texture
+newOreType.forEach(ore => {
+  newRockType.forEach(async rock => {
 
     let oreImage = ore.isGraded ? `./base/ores/normal_${ore.name}.png` : `./base/ores/${ore.name}.png`
     let rockImage = `./base/rocks/${rock.name}.png`
@@ -317,11 +103,79 @@ oreType.forEach(ore => {
       opacitySource: 1,
     })
 
-    const outpueImage = `../kubejs/assets/kubejs/textures/block/ore/${ore.name}/${rock.name}.png`
-    base.write(outpueImage, (err) => {
+    const outputImage = `../kubejs/assets/kubejs/textures/block/ore/${ore.name}/${rock.name}.png`
+    base.write(outputImage, (err) => {
       if (err) throw err
-      console.log("Image composition successful. Output saved at", outpueImage)
+      console.log("Image composition successful. Output saved at", outputImage)
     })
     
+  })
+})
+
+// generate worldgen config
+newOreType.forEach(ore => {
+  newRockType.forEach(async rock => {
+    let filePath = `../config/adlods/Deposits/_${ore.name}_${rock.name}.cfg`
+    let enabled = false
+    let veinData = [256000, 0, 0, 1, 1]
+    if (ore.veinData[rock.dimension]) {
+      enabled = true
+      veinData = ore.veinData[rock.dimension]
+    }
+    let configData = `
+      Config {
+          B:enabled=${enabled}
+      }
+      Deposit {
+          S:customReplacements <
+          >
+          S:ores <
+              kubejs:ore/${ore.name}/${rock.name}
+          >
+          I:rarity=${veinData[0]}
+          S:replaceableBlocks <
+              ${rock.origin}
+          >
+          Dimensions {
+              S:blackList <
+              >
+              S:whiteList <
+                  ${rock.dimension}
+              >
+          }
+          Biomes {
+              S:blackList <
+              >
+              S:whiteList <
+              >
+          }
+          Altitude {
+              I:max=${veinData[1]}
+              I:min=${veinData[2]}
+          }
+          Miscellaneous {
+              B:exposed=true
+              S:proportions=0.5
+          }
+          Size {
+              I:max=${veinData[3]}
+              I:min=${veinData[4]}
+          }
+          Indicator {
+              S:circles <
+                  tfc:ore/small_${ore.name}, 3
+                  tfc:ore/small_${ore.name}, 9
+                  tfc:ore/small_${ore.name}, 15
+              >
+              S:continuity=20.0
+              I:distortion=1
+              S:id=
+          }
+      }
+    `
+    fs.writeFile(filePath, configData, (err) => {
+      if (err) throw err
+      console.log("Config creation successful. Output saved at", filePath)
+    })
   })
 })
