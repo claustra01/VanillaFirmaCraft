5f5f341e8edfef7c6a94a474deb33e9b319785df
claustra01 <shinya430.g@gmail.com>
2023-08-18 19:11:41 +0900

    :sparkles: replace animal drops

---
+++ kubejs/startup_scripts/ref/utils.js
@@ -0,0 +1,18 @@
+global.colors = [
+  "white",
+  "orange",
+  "magenta",
+  "light_blue",
+  "yellow",
+  "lime",
+  "pink",
+  "gray",
+  "light_gray",
+  "cyan",
+  "purple",
+  "blue",
+  "brown",
+  "greed",
+  "red",
+  "black"
+]
