bc1ca075b41eb27e5aca8171c4b57c3ea2707471
claustra01 <shinya430.g@gmail.com>
2024-01-28 18:21:39 +0900

    add: hide vanilla tools and wood blocks

---
+++ kubejs/client_scripts/hide_items/minecraft.js
@@ -0,0 +1,40 @@
+console.info('Hide Vanilla items...')
+
+JEIEvents.hideItems(event => {
+
+  // tile entities
+  global.vanillaTileEntities.forEach(tile => {
+    event.hide(tile)
+  })
+
+  // vanilla tools
+  for (const material of ["wooden", "stone", "iron", "diamond", "netherite", "golden"]) {
+    for (const tool of ["shovel", "pickaxe", "axe", "hoe", "sword"]) {
+      event.hide(`minecraft:${material}_${tool}`) 
+    }
+  }
+  for (const material of ["iron", "diamond", "netherite", "golden"]) {
+    for (const armor of ["helmet", "chestplate", "leggings", "boots"]) {
+      event.hide(`minecraft:${material}_${armor}`) 
+    }
+  }
+
+  // wood blocks
+	global.tfcWoodType.forEach(wood => {
+    if (wood.replace == true) {
+      event.hide(`minecraft:${wood.vanilla}_planks`)
+      event.hide(`minecraft:${wood.vanilla}_slab`)
+      event.hide(`minecraft:${wood.vanilla}_stairs`)
+      event.hide(`minecraft:${wood.vanilla}_fence`)
+      event.hide(`minecraft:${wood.vanilla}_sign`)
+      event.hide(`minecraft:${wood.vanilla}_button`)
+      event.hide(`minecraft:${wood.vanilla}_pressure_plate`)
+      event.hide(`minecraft:${wood.vanilla}_door`)
+      event.hide(`minecraft:${wood.vanilla}_trapdoor`)
+      event.hide(`minecraft:${wood.vanilla}_fence_gate`)
+      event.hide(`minecraft:${wood.vanilla}_boat`)
+      event.hide(`minecraft:${wood.vanilla}_chest_boat`)
+    }
+	})
+
+})
---
+++ kubejs/startup_scripts/global/minecraft.js
@@ -0,0 +1,15 @@
+global.vanillaTileEntities = [
+  "minecraft:crafting_table",
+  "minecraft:furnace",
+  "minecraft:chest",
+  "minecraft:loom",
+  "minecraft:composter",
+  "minecraft:barrel",
+  "minecraft:smoker",
+  "minecraft:blast_furnace",
+  "minecraft:cartography_table",
+  "minecraft:fletching_table",
+  "minecraft:grindstone",
+  "minecraft:smithing_table",
+  "minecraft:stonecutter"
+]
