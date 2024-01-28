6e765896e353a2cb6bbfbcba4908e9f4482de65b
claustra01 <shinya430.g@gmail.com>
2024-01-28 19:12:32 +0900

    add: remove vanilla deviced recipe categories

---
+++ kubejs/client_scripts/hide_categories.js
@@ -0,0 +1,12 @@
+console.info('Hide JEI Categories...')
+
+JEIEvents.removeCategories(event => {
+  event.remove("minecraft:blasting")
+  event.remove("minecraft:campfire")
+  event.remove("minecraft:compostable")
+  event.remove("minecraft:fuel")
+  event.remove("minecraft:furnace")
+  event.remove("minecraft:smithing")
+  event.remove("minecraft:smoking")
+  event.remove("minecraft:stonecutting")
+})
