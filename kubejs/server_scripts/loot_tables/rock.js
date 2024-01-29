c3adf46eeb433436d2bb0540bfac663bd2c702f7
claustra01 <shinya430.g@gmail.com>
2024-01-29 13:22:28 +0900

    add: replace stone drops

---
+++ kubejs/server_scripts/loot_tables/rock.js
@@ -0,0 +1,47 @@
+LootJS.modifiers((event) => {
+  // stone
+  event
+    .addBlockLootModifier("minecraft:stone")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/dacite")
+  event
+    .addBlockLootModifier("minecraft:cobblestone")
+    .removeLoot(Ingredient.all)
+    .addLoot("tfc:rock/cobble/dacite")
+  event
+    .addBlockLootModifier("minecraft:mossy_cobblestone")
+    .removeLoot(Ingredient.all)
+    .addLoot("tfc:rock/mossy_cobble/dacite")
+
+  // granite
+  event
+    .addBlockLootModifier("minecraft:granite")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/granite")
+  // diorite
+  event
+    .addBlockLootModifier("minecraft:diorite")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/diorite")
+  // andesite
+  event
+    .addBlockLootModifier("minecraft:andesite")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/andesite")
+
+  // basalt
+  event
+    .addBlockLootModifier("minecraft:basalt")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/basalt")
+  event
+    .addBlockLootModifier("minecraft:smooth_basalt")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/basalt")
+
+  // calcite
+  event
+    .addBlockLootModifier("minecraft:calcite")
+    .removeLoot(Ingredient.all)
+    .addWeightedLoot([1, 4], "tfc:rock/loose/chalk")
+})
