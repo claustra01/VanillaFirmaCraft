88c6d483b7ab1b65f69b8d69cbc48a176c5fad99
claustra01 <shinya430.g@gmail.com>
2024-01-30 21:15:02 +0900

    add: tag and recipes to vanilla food items

---
+++ kubejs/server_scripts/tfc/food_items/minecraft.js
@@ -0,0 +1,73 @@
+TFCEvents.data(event => {
+
+  // apple
+  event.foodItem('minecraft:apple', food => {
+    food.hunger(4)
+    food.saturation(0.5)
+    food.decayModifier(1.7)
+    food.fruit(1)
+  })
+
+  // melon_slice
+  event.foodItem('minecraft:melon_slice', food => {
+    food.hunger(4)
+    food.saturation(0.2)
+    food.water(5)
+    food.decayModifier(2.5)
+    food.fruit(0.75)
+  })
+
+  // sweet_berries
+  event.foodItem('minecraft:sweet_berries', food => {
+    food.hunger(4)
+    food.saturation(0.2)
+    food.water(5)
+    food.decayModifier(4.9)
+    food.fruit(1)
+  })
+
+  // glow_berries
+  event.foodItem('minecraft:glow_berries', food => {
+    food.hunger(4)
+    food.saturation(0.5)
+    food.water(5)
+    food.decayModifier(4.9)
+    food.fruit(0.75)
+  })
+
+  // carrot
+  event.foodItem('minecraft:carrot', food => {
+    food.hunger(4)
+    food.saturation(2)
+    food.decayModifier(0.7)
+    food.vegetables(1)
+  })
+
+  // potato
+  event.foodItem('minecraft:potato', food => {
+    food.hunger(4)
+    food.saturation(2)
+    food.decayModifier(0.666)
+    food.vegetables(1.5)
+  })
+
+  // beet
+  event.foodItem('minecraft:beetroot', food => {
+    food.hunger(4)
+    food.saturation(2)
+    food.decayModifier(0.7)
+    food.vegetables(1)
+  })
+
+  // no nutrition foods
+  event.foodItem('minecraft:wheat', food => {
+    food.decayModifier(2)
+  })
+  event.foodItem('minecraft:poisonous_potato', food => {
+    food.decayModifier(0.666)
+  })
+  event.foodItem('minecraft:chorus_fruit', food => {
+    food.decayModifier(4.9)
+  })
+
+})
