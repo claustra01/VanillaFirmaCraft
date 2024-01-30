0a15a7da89c8c58993226732ef0b29d1bf04f365
claustra01 <shinya430.g@gmail.com>
2024-01-30 18:01:41 +0900

    chore: hide tfc plants

--- kubejs/startup_scripts/global/tfc_plant.js
+++ kubejs/startup_scripts/global/tfc_plant.js
@@ -121,6 +121,7 @@ global.tfcPlants = [
   "tfc:plant/saguaro",
   "tfc:plant/sea_pickle",
   "tfc:plant/aspen_krummholz",
+  "tfc:plant/white_cedar_krummholz",
   "tfc:wild_crop/barley",
   "tfc:wild_crop/oat",
   "tfc:wild_crop/rye",
