6351643b662fb5c2aaaa19d04d9b28bc28dd5b5a
claustra01 <shinya430.g@gmail.com>
2024-02-01 20:05:30 +0900

    update: kaolin clay prospectable

--- kubejs/server_scripts/recipes/tfc_rocks.js
+++ kubejs/server_scripts/recipes/tfc_rocks.js
@@ -113,3 +113,13 @@ ServerEvents.tags('item', (event) => {
   // flux
   event.add('tfc:fluxstone', 'rocks:seashell');
 });
+
+ServerEvents.tags('block', (event) => {
+  // kaolin clay
+  event.add('tfc:prospectable', 'tfc:white_kaolin_clay');
+  event.add('tfc:prospectable', 'tfc:pink_kaolin_clay');
+  event.add('tfc:prospectable', 'tfc:red_kaolin_clay');
+  event.add('precisionprospecting:prospectable_mineral', 'tfc:white_kaolin_clay');
+  event.add('precisionprospecting:prospectable_mineral', 'tfc:pink_kaolin_clay');
+  event.add('precisionprospecting:prospectable_mineral', 'tfc:red_kaolin_clay');
+});
