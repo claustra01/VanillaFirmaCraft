2aa824796bafef2c2550934b6c61487739a1a036
claustra01 <shinya430.g@gmail.com>
2024-02-03 15:46:49 +0900

    add: custom loose rock knapping recipes

--- kubejs/server_scripts/recipes/tfc_rocks.js
+++ kubejs/server_scripts/recipes/tfc_rocks.js
@@ -1,6 +1,36 @@
 console.info('Tweak TFC rock recipes...');
 
 ServerEvents.tags('item', (event) => {
+  // loose rocks
+  event.add('tfc:rock_knapping', [
+    'kubejs:loose_deepslate_rock',
+    'kubejs:loose_netherrack_rock',
+    'kubejs:loose_blackstone_rock',
+    'kubejs:loose_end_stone_rock'
+  ]);
+
+  event.add('tfc:igneous_intrusive_rock', [
+    'kubejs:loose_deepslate_rock',
+    'kubejs:loose_netherrack_rock',
+  ]);
+  event.add('tfc:igneous_extrusive_rock', [
+    'kubejs:loose_blackstone_rock',
+  ]);
+  event.add('tfc:metamorphic_rock', [
+    'kubejs:loose_end_stone_rock'
+  ]);
+
+  event.add('tfc:igneous_intrusive_items', [
+    'kubejs:loose_deepslate_rock',
+    'kubejs:loose_netherrack_rock',
+  ]);
+  event.add('tfc:igneous_extrusive_items', [
+    'kubejs:loose_blackstone_rock',
+  ]);
+  event.add('tfc:metamorphic_items', [
+    'kubejs:loose_end_stone_rock'
+  ]);
+
   // flux
   event.add('tfc:fluxstone', 'rocks:seashell');
 });
